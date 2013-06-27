1bd715db11140b436f73b52460263ce8250e5382
dlc <davidl@tribalddb.co.uk>
2013-06-27 17:49:42 +0100

    Added code using api. ** Requires x86 version of JDK

---
+++ test/unit/com/lewiscrosby/music/ApiControllerSpec.groovy
@@ -0,0 +1,17 @@
+package com.lewiscrosby.music
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(ApiController)
+class ApiControllerSpec {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
